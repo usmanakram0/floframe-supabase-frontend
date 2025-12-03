abff42b06d47ab48a0758e861ff632b77dd40a78
Usman Akram <usmanakram4118@gmail.com>
2025-12-03 20:14:37 +0500

    pushed by adding validations on reset time of tier limit

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import App from "./App.tsx";
 import "./index.css";
 import { AuthProvider } from "@/contexts/AuthContext";
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")).render(
   <AuthProvider>
     <App />
   </AuthProvider>
